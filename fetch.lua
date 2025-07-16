39862f1da295b84559b1c7682a78df2d1a537919
discoart <vanishvec@gmail.com>
2025-07-16 20:29:58 +0300

    Update fetch.lua

--- fetch.lua
+++ fetch.lua
@@ -2,4 +2,18 @@
 
 return {
   version = '1.0'
+  codes =  {
+    "777",
+    "ThreeBeeVee",
+    "15MMembers",
+    "BeesBuzz123",
+    "ClubBean",
+    "38217",
+    "Bopmaster",
+    "Connoisseur",
+    "Crawlers",
+    "Nectar",
+    "Roof",
+    "Wax"
+  }
 }
